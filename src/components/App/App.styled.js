612ca6cd4c5ed0660f17f77ef83cbdb11a8dfdd0
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-05 22:46:50 +0100

    Create App.styled.js

---
+++ src/components/App/App.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 500px;
+  padding: 15px;
+  margin: 0 auto;
+`;
+export const Title = styled.h1`
+  text-align: center;
+  margin-top: 0;
+`;
+export const TitleTwo = styled.h2`
+  text-align: center;
+`;
