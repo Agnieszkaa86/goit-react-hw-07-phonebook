47f52d2c7d1c85311f66d6c010929108c1ca4696
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-05 22:46:40 +0100

    Create Filter.styled.js

---
+++ src/components/Filter/Filter.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const Input = styled.input`
+  display: block;
+  margin-top: 15px;
+  margin-bottom: 20px;
+`
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  font-weight: 500;
+  font-size: 15px;
+`;
